87f6a24b577addc266b5e4b6145e30b3531ab8b4
sean-roberts354 <sean.roberts354@gmail.com>
2026-06-24 15:46:21 -0400

    Create optional verbose argument to let user decide how much information to display

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main() -> None:
     load_dotenv()
@@ -11,21 +12,32 @@ def main() -> None:
         print("Unable to load API key")
 
     client = genai.Client(api_key=api_key)
+
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
     args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
 
     response: object = client.models.generate_content(
                                     model="gemini-2.5-flash",
-                                    contents=args.user_prompt)
+                                    contents=messages)
     
     if response.usage_metadata is None:
         raise RuntimeError("Missing usage metadata; Likely failed API request")
-    
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("-----------")    
-    print(response.text)
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("-----------")    
+        print(response.text)
+    elif not args.verbose:
+        print(response.text)
 
 
 if __name__ == "__main__":
