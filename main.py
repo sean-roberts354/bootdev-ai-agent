486a165a8efb3e422c8d6673f9d7bc0256c692a6
sean-roberts354 <sean.roberts354@gmail.com>
2026-06-24 14:42:26 -0400

    Display token usage for each response

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ except RuntimeError:
 client = genai.Client(api_key=api_key)
 
 def main():
-    generated_content: object = client.models.generate_content(
+    response: object = client.models.generate_content(
                                     model="gemini-2.5-flash",
                                     contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     
-    print(generated_content.text)
+    if response.usage_metadata == None:
+        raise RuntimeError("Missing usage metadata; Likely failed API request")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("-----------")    
+    print(response.text)
 
 
 if __name__ == "__main__":
