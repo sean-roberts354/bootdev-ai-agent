46c9ac1bbb292a1dec2efab12fecd5477a2ce852
sean-roberts354 <sean.roberts354@gmail.com>
2026-06-24 15:18:22 -0400

    Import argparse library to allow users to enter their own prompt

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-try:
-    api_key = os.environ.get("GEMINI_API_KEY")
-except RuntimeError:
-    print("Unable to load API key")
+def main() -> None:
+    load_dotenv()
+    try:
+        api_key = os.environ.get("GEMINI_API_KEY")
+    except RuntimeError:
+        print("Unable to load API key")
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
-def main():
     response: object = client.models.generate_content(
                                     model="gemini-2.5-flash",
-                                    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+                                    contents=args.user_prompt)
     
-    if response.usage_metadata == None:
+    if response.usage_metadata is None:
         raise RuntimeError("Missing usage metadata; Likely failed API request")
     
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
