c65ccfdf85aee5880352601d14dcc2363165ffcb
sean-roberts354 <sean.roberts354@gmail.com>
2026-06-24 13:13:34 -0400

    Setup basic AI client with hard-coded prompts to test API key

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+try:
+    api_key = os.environ.get("GEMINI_API_KEY")
+except RuntimeError:
+    print("Unable to load API key")
+
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from bootdev-ai-agent!")
+    generated_content: object = client.models.generate_content(
+                                    model="gemini-2.5-flash",
+                                    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    print(generated_content.text)
 
 
 if __name__ == "__main__":
